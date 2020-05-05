e5b731a3606a65d9175efeff2a7501db2d8d83e1
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-05-05 15:39:05 -0400

    Failed to fetch error

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    '/transaction.js',
+    '../models/transaction.js',
     '/index.js',
 
   ];
